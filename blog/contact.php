c132fa82ef70022d624fb24f459f252adccabd5e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-09-14 22:06:51 +0530

    Implement contact php page

---
+++ blog/contact.php
@@ -0,0 +1,18 @@
+<?php
+include 'partials/header.php';
+?>
+
+<!-- End Nav Bar -->
+
+<!-- About Section -->
+
+<section class="empty__page">
+    <h1>Contact Page</h1>
+</section>
+
+<!-- End About Section -->
+
+<!-- Start Footer Section -->
+<?php
+include 'partials/footer.php';
+?>
