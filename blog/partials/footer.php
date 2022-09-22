4edde399a7f7ede9340a43197a736f960e1b981d
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-09-23 01:58:03 +0530

    complete header and footer section

---
+++ blog/partials/footer.php
@@ -0,0 +1,63 @@
+<!-- Add Repeating Footer Section -->
+<footer>
+    <div class="footer__socials">
+        <a href="#" target="_blank" aria-label="Youtube"><i class="uil uil-youtube"></i></a>
+        <a href="#" target="_blank" aria-label="Facebook"><i class="uil uil-facebook-f"></i></a>
+        <a href="#" target="_blank" aria-label="Instagram"><i class="uil uil-instagram-alt"></i></a>
+        <a href="#" target="_blank" aria-label="Linkedin"><i class="uil uil-linkedin"></i></a>
+        <a href="#" target="_blank" aria-label="Twitter"><i class="uil uil-twitter"></i></a>
+    </div>
+
+    <div class="container footer__container">
+        <article>
+            <h4>Categories</h4>
+            <ul>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Wild Life</a></li>
+                <li><a href="#">Travels</a></li>
+                <li><a href="#">Foods</a></li>
+                <li><a href="#">Films</a></li>
+                <li><a href="#">Technology</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Supports</h4>
+            <ul>
+                <li><a href="#">Online Support</a></li>
+                <li><a href="#">Emaols</a></li>
+                <li><a href="#">Call Numbers</a></li>
+                <li><a href="#">Locations</a></li>
+                <li><a href="#">Social Media</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="#">Recent</a></li>
+                <li><a href="#">Popular</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">Safety</a></li>
+                <li><a href="#">Games</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__copyright">
+        <small>Copyright &copy; Thirosh Madhusha 2022</small>
+    </div>
+
+</footer>
+
+<script src="<?= ROOT_URL ?>js/main.js"></script>
+</body>
+
+</html>
